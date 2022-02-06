d4bd2cc0d27c50e29d73816700c2f386557411d9
清水啓太郎 <>
2022-02-06 22:08:50 +0900

    Implemented callback from unity side

--- react_unity/src/App.js
+++ react_unity/src/App.js
@@ -1,7 +1,8 @@
-import logo from './logo.svg';
+
+import React, { useState, useEffect } from "react";
 import './App.css';
 
-import React from "react";
+//import React from "react";
 import Unity, { UnityContext } from "react-unity-webgl";
 
 const unityContext = new UnityContext({
@@ -15,6 +16,20 @@ const unityContext = new UnityContext({
 
 
 function App() {
+
+
+  const [direction, setDirection] = useState("");
+  const [xpos, setXpos] = useState(0);
+  const [ypos, setYpos] = useState(0);
+
+  useEffect(function () {
+    unityContext.on("MoveCallback", function (direction,xpos, ypos) {
+      setDirection(direction);
+      setXpos(xpos);
+      setYpos(ypos);
+    });
+  }, []);
+
   function moveRight() {
     unityContext.send("Sphere", "MoveRight", 10);
   }
@@ -22,11 +37,13 @@ function App() {
     unityContext.send("Sphere", "MoveLeft", 10);
   }
 
+  
+
   return (
     <div>
       <button onClick={moveRight}>MoveRight</button>
       <button onClick={moveLeft}>MoveLeft</button>
-
+      {<p>{`Moved! ${direction} x = ${xpos} y = ${ypos} `}</p>}
       <Unity unityContext={unityContext} 
         style={{
           height: "100%",
